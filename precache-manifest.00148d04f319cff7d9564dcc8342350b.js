46445f5505e12b8af587e796c0a32fd775f45196
Faria Begum Riya <faria.ayir@gmail.com>
2019-08-30 16:52:52 +0600

    Updates

---
+++ precache-manifest.00148d04f319cff7d9564dcc8342350b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e610a3b1d51f3b3afc7680366beeb660",
+    "url": "/pop-quiz/index.html"
+  },
+  {
+    "revision": "8353b4f79bbd714bb61c",
+    "url": "/pop-quiz/static/css/main.ba09124f.chunk.css"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/pop-quiz/static/js/2.be491123.chunk.js"
+  },
+  {
+    "revision": "8353b4f79bbd714bb61c",
+    "url": "/pop-quiz/static/js/main.38c563e6.chunk.js"
+  },
+  {
+    "revision": "d4c74925da1a453b20a8",
+    "url": "/pop-quiz/static/js/runtime~main.402f6919.js"
+  }
+]);
